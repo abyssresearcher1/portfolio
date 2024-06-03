0251127dc0d35d9dd4640732676755da6609dc2a
abyssresearcher1 <akylbekashyralie@gmail.com>
2024-06-03 19:49:59 +0600

    added tagloud sphere and some design

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import image from "../../assets/img/MYface.jpg";
 import Layout from "../../Layouts/Layout";
 import styles from "./MainPage.module.css";
+import cn from "classnames";
+import TagCloud from "TagCloud";
 
 const MainPage = () => {
-  const btnState = {
-    display: "flex",
+  const [isImageVisible, setIsImageVisible] = useState(true);
+  const [isStackVisible, setIsStackVisible] = useState(false);
+  const textSphereRef = useRef(null);
+
+  const toggleFunction = () => {
+    setIsStackVisible(!isStackVisible);
+    setIsImageVisible(!isImageVisible);
   };
-  const toggleFunction = () => {};
+
+  useEffect(() => {
+    const container = textSphereRef.current;
+    const texts = [
+      "HTML",
+      "CSS",
+      "BEM",
+      "SASS",
+      "JavaScript",
+      "Jquery",
+      "React",
+      "ant-design",
+      "React-router",
+      "React-hook-form",
+      "ES6",
+      "GIT",
+      "GITHUB",
+      "Adaptive-layout",
+      "Axios"
+    ];
+
+    const options = {
+      radius: 250,
+      maxSpeed: "fast",
+      initSpeed: "fast",
+      keep: true,
+    };
+
+    if (container) {
+      TagCloud(container, texts, options);
+    }
+  }, [0]);
 
   return (
     <Layout>
@@ -16,7 +54,7 @@ const MainPage = () => {
           <div className={styles.mainpage_info}>
             <div className={styles.info_general}>
               <p className={styles.hello_text}>Здравствуйте! Меня зовут</p>
-              <h2>Акылбек </h2>
+              <h2>Акылбек</h2>
               <p>и я начинающий</p>
               <h1>Front-end developer</h1>
             </div>
@@ -26,13 +64,29 @@ const MainPage = () => {
                 так же ознакомитсья с моим резюме
               </p>
             </div>
-            <div className={styles.mainpage_btn}>
+            <div className={styles.mainpage_btn} onClick={toggleFunction}>
               <span>Показать стек</span>
               <i></i>
             </div>
           </div>
-          <div className={styles.mainpage_img}>
-            <img src={image} alt="face-image" className={styles.face_image} />
+          <div
+            className={styles.mainpage_img}
+            style={{ position: "relative", width: "500px", height: "500px" }}
+          >
+            <img
+              src={image}
+              alt="face-image"
+              className={cn(styles.face_image, {
+                [styles.face_image_hidden]: !isImageVisible,
+              })}
+            />
+            <div
+              className={cn(styles.stack_div, {
+                [styles.stack_visible]: isStackVisible,
+              })}
+            >
+              <div ref={textSphereRef} className={styles.text_sphere}></div>
+            </div>
           </div>
         </div>
       </div>
