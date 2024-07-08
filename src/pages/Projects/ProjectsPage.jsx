be521a76632dc6d0dc50c31935934bc1e339ba77
abyssresearcher1 <akylbekashyralie@gmail.com>
2024-07-08 22:13:38 +0600

    changed colors

--- src/pages/Projects/ProjectsPage.jsx
+++ src/pages/Projects/ProjectsPage.jsx
@@ -11,36 +11,63 @@ import aalam from "../../assets/img/aalam_screen.jpg";
 import lens from "../../assets/img/3d-lens.jpg";
 import parallax from "../../assets/img/parallax-forest.jpg";
 import unistore from "../../assets/img/unistore.jpg";
+import redux from "../../assets/img/redux-todolist.jpg";
+import pokedex from "../../assets/img/pokedex.jpg";
+import weather from "../../assets/img/weather-app.jpg";
 
 const ProjectsPage = () => {
   const projects = [
     {
-      name: "aalam.kg",
+      name: "Aalam.kg",
       img: aalam,
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, dicta!",
+      "первая работа и коммерческий опыт",
       link: "adasdasd",
     },
     {
       name: "3d-lens",
       img: lens,
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, dicta!",
+        "проект для ютуба",
       link: "dasdasdasdas",
     },
     {
-      name: "fairy forest",
+      name: "Fairy forest",
       img: parallax,
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, dicta!",
+        "проект для ютуба",
       link: "asdasdasd",
     },
     {
-      name: "unistore",
+      name: "Unistore",
       img: unistore,
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, dicta!",
+        "вторая работа",
       link: "asdasdasda",
+    },
+  ];
+
+  const petProjects = [
+    {
+      name: "Redux-todolist",
+      img: redux,
+      description:
+        "Todolist с использованием redux",
+      link: "dasdasdasd",
+    },
+    {
+      name: "Pokedex",
+      img: pokedex,
+      description:
+        "pokedex",
+      link: "dasdasdasd",
+    },
+    {
+      name: "Weather-app",
+      img: weather,
+      description:
+        "приложение погоды",
+      link: "dasdasdasd",
     },
   ];
 
@@ -61,11 +88,11 @@ const ProjectsPage = () => {
                   home
                 </span>
                 <span className={styles.files_about}>
-                  <VscFolderOpened />
+                  <VscFolder />
                   about
                 </span>
                 <span className={styles.files_projects}>
-                  <VscFolder />
+                  <VscFolderOpened />
                   projects
                 </span>
                 <span className={styles.files_pret}>
@@ -84,14 +111,18 @@ const ProjectsPage = () => {
             </div>
             <div className={styles.projects_info}>
               <div className={styles.info_head}>
-                <span>
-                  <FaReact />
-                  About.jsx
-                </span>
-                <span>
-                  <FaReact /> 
-                  Projects.jsx
-                </span>
+                <div className={styles.info_head_tabs}>
+                  <div className={styles.info_head_about}>
+                    <span>
+                      <FaReact />
+                      About.jsx
+                    </span>
+                  </div>
+                  <div className={styles.info_head_projects}>
+                    <FaReact />
+                    Projects.jsx
+                  </div>
+                </div>
               </div>
               <div className={styles.com_projects_title}>
                 <h2>Коммерческие проекты</h2>
@@ -118,7 +149,25 @@ const ProjectsPage = () => {
               <div className={styles.pet_projects_title}>
                 <h2>Пет проекты</h2>
               </div>
-              <div className={styles.pet_projects_cards}></div>
+              <div className={styles.pet_projects_cards}>
+                <div className={styles.cards}>
+                  {petProjects &&
+                    petProjects.map((item) => {
+                      return (
+                        <div className={styles.card}>
+                          <img src={item.img} alt="images" />
+                          <span className={styles.card_name}>{item.name}</span>
+                          <p className={styles.cards_description}>
+                            {item.description}
+                          </p>
+                          <button className={styles.card_btn}>
+                            <a href={item.link}>Проект</a>
+                          </button>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
             </div>
           </div>
         </div>
